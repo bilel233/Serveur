6c2c1e5f8e8f58df3e5122179b5923df148101d9
bilel233 <bilel.mjdb@gmail.com>
2024-01-16 19:24:42 +0100

    Projet instancié

---
+++ API_les_bon_comptes_les_bons_amis/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace API_les_bon_comptes_les_bons_amis
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
