fd173b95f75b1443449a56fa754b2126a1283564
bilel233 <bilel.mjdb@gmail.com>
2024-01-16 22:13:40 +0100

    connection a mongoDB puis recuperation des elements dans la collection

--- API_les_bon_comptes_les_bons_amis/Program.cs
+++ API_les_bon_comptes_les_bons_amis/Program.cs
@@ -1,3 +1,35 @@
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+MongoClient client = new MongoClient("mongodb+srv://BilelMajdoub:Mongodb1998%40@bdd.uquucse.mongodb.net/");
+
+List<string> databases = client.ListDatabaseNames().ToList();
+
+for (int i = 0; i < databases.Count; i++)
+{
+    string dbName = databases[i];
+    Console.WriteLine(dbName);
+}
+
+// Obtenez une référence à la base de données 'sample_mflix'
+var database = client.GetDatabase("sample_mflix");
+
+// Obtenez une référence à la collection 'users'
+var collection = database.GetCollection<BsonDocument>("users");
+
+// Créez un filtre pour sélectionner tous les documents
+var filter = new BsonDocument();
+
+// Utilisez la méthode Find avec le filtre pour obtenir tous les documents de la collection
+var documents = collection.Find(filter).ToList();
+
+// Parcourez et imprimez tous les documents
+foreach (var document in documents)
+{
+    Console.WriteLine(document.ToString());
+}
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
